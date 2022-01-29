870336a9ce9cba1c04da34c684f5c3e889182945
Anshul BANSAL <anshul.bansal@vivacredit.ro>
2022-01-29 20:58:21 +0530

    code change

--- chapter2/todo/src/main/java/io/educative/domains/TodoType.java
+++ chapter2/todo/src/main/java/io/educative/domains/TodoType.java
@@ -1,19 +1,28 @@
 package io.educative.domains;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import io.educative.utils.adapters.xml.DataFormatXmlAdapter;
 import lombok.Data;
 
-import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.*;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import java.util.Date;
 
 @Data
-@XmlRootElement
 @JsonPropertyOrder(alphabetic = true)
+@XmlRootElement(name="type")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(propOrder = { "code", "dateCreated", "lastUpdated" })
 public class TodoType {
 
     private String code;
-    private String description;
-    private Date dateCreated;
-    private Date lastUpdated;
 
+    @XmlTransient
+    private String description;
+
+    @XmlJavaTypeAdapter(DataFormatXmlAdapter.class)
+    private Date dateCreated;
+
+    @XmlJavaTypeAdapter(DataFormatXmlAdapter.class)
+    private Date lastUpdated;
 }
