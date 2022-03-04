4117d809464e5bd0bbc1b65c5383298c14a1090d
Anshul BANSAL <anshul.bansal@vivacredit.ro>
2022-03-04 18:24:27 +0530

    Code change

--- chapter4/todo/src/main/java/io/educative/controllers/TodoRestController.java
+++ chapter4/todo/src/main/java/io/educative/controllers/TodoRestController.java
@@ -5,8 +5,6 @@ import io.educative.repositories.TodoRestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.Link;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,12 +13,12 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RepositoryRestController
-public class TodoController {
+public class TodoRestController {
 
     private final TodoRestRepository repository;
 
     @Autowired
-    public TodoController(TodoRestRepository repo) {
+    public TodoRestController(TodoRestRepository repo) {
         repository = repo;
     }
 
@@ -28,7 +26,7 @@ public class TodoController {
     public @ResponseBody ResponseEntity<?> getTodos() {
         Iterable<Todo> todos = repository.findAll();
         CollectionModel<Todo> resources = CollectionModel.of(todos);
-        resources.add(linkTo(methodOn(TodoController.class).getTodos()).withSelfRel());
+        resources.add(linkTo(methodOn(TodoRestController.class).getTodos()).withSelfRel());
         return ResponseEntity.ok(resources);
     }
 
