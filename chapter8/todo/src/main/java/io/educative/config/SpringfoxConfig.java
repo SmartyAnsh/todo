9af93ff15ce6988ab6874d43745b7e8a0bac3e98
Anshul BANSAL <anshul.bansal@vivacredit.ro>
2022-05-07 14:27:56 +0300

    Chapter 8 code - SpringfoxConfig

--- chapter8/todo/src/main/java/io/educative/config/SpringfoxConfig.java
+++ chapter8/todo/src/main/java/io/educative/config/SpringfoxConfig.java
@@ -8,7 +8,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-public class SpringFoxConfig {
+public class SpringfoxConfig {
 
     @Bean
     public Docket api() {
